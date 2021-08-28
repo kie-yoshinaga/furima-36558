4b7c06f601594f99c96748450b855e21c65a49a5
kie-yoshinaga <kie.103.eiwa@gmail.com>
2021-08-28 20:45:45 +0900

    商品を登録できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,18 @@
 class ItemsController < ApplicationController
 
-  #def index
-    #@items = Item.all
-  #end
+  def index
+    @items = Item.all
+  end
 
+  def new
+    @items = Item.new
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+  
 end
---
+++ app/models/article.rb
@@ -0,0 +1,16 @@
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  validates :title, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :postage_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :shipping_date_id, numericality: { other_than: 1, message: "can't be blank"} 
+end
