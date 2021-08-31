465c30bd8764b86fd75e9e9cefba96c982a47908
kie-yoshinaga <kie.103.eiwa@gmail.com>
2021-08-31 12:34:32 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
+  before_action :move_to_index, only: [:edit]
 
 
   def index
@@ -24,12 +25,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    #item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image,:title,:catch_copy,:category_id,:condition_id,:price,:postage_id,:prefecture_id,:shipping_date_id).merge(user_id: current_user.id)
   end
 
-  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveHash::Base
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: 'レディース' },
