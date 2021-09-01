108acd50c51643cbc77ff1035422588bc63ed1d0
kie-yoshinaga <kie.103.eiwa@gmail.com>
2021-09-01 12:37:03 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new, :edit, :update]
-  before_action :set_message, only: [:show, :edit, :update ]
-  before_action :redirect_root, only: [:edit, :update ]
+  before_action :set_message, only: [:show, :edit, :update, :destroy ]
+  before_action :redirect_root, only: [:edit, :update, :destroy ]
   
 
 
@@ -35,6 +35,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
